947112430796257063ce7f8d6eafd25fe78fa167
hysong4u <hayeon_song@naver.com>
2024-02-19 08:48:25 +0900

    [feat] 오타 수정

--- src/main/java/com/example/comma/domain/fairytale/dto/FairytaleResponseDto.java
+++ src/main/java/com/example/comma/domain/fairytale/dto/FairytaleResponseDto.java
@@ -3,7 +3,7 @@ package com.example.comma.domain.fairytale.dto;
 public record FairytaleResponseDto(
         Long id,
         String title,
-        String imgaeUrl,
+        String imageUrl,
         String channelName,
         String year,
         String time,
