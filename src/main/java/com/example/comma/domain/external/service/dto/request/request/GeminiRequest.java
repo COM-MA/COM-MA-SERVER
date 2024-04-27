2d87793ed847ea5e23a0514b38bf97f1d0e67980
hysong4u <hayeon_song@naver.com>
2024-04-28 01:40:23 +0900

    [fix] dto 삭제

--- src/main/java/com/example/comma/domain/external/service/dto/request/request/GeminiRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.comma.domain.external.service.dto.request.request;
-
-public class GeminiRequest {
-    private String text;
-
-    public GeminiRequest(String text) {
-        this.text = text;
-    }
-}
--- src/main/java/com/example/comma/domain/external/service/dto/response/GeminiResponse.java
+++
@@ -1,9 +0,0 @@
-package com.example.comma.domain.external.service.dto.response;
-
-import lombok.Data;
-
-@Data
-public class GeminiResponse {
-    private boolean success;
-    private byte[] imageData;
-}
